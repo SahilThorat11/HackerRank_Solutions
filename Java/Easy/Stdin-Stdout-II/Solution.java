2daecc2ee7f045d0b701767671b76af2cdceb834
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-11 11:05:01 +0530

    Input-Output - II

---
+++ Java/Easy/Stdin-Stdout-II/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo = sobj.nextInt();
+        double dNo = sobj.nextDouble();
+
+        sobj.nextLine();
+
+        String str = sobj.nextLine();
+
+        System.out.println("String: " + str);
+        System.out.println("Double: " + dNo);
+        System.out.println("Int: " + iNo);
+
+        sobj.close();
+    }
+}
